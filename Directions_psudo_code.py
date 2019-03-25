fbdf1826c5d21812c1d5dfc817e3f8aa0fb36b20
SWLNY <lynchsusanw@gmail.com>
2019-03-25 18:57:22 -0400

    Upload third program in Exercise 1

---
+++ Directions_psudo_code.py
@@ -0,0 +1,22 @@
+"""
+
+This code is used to travel from NYBG to Tuckahoe, NY
+
+"""
+to_address = "Tuckahoe, NY"
+print('Is it 6:00 pm or later?')
+print('If earlier than 6:00 pm, do more work')
+print('If 6:00 or later, check the Bronx traffic map')
+print('If traffic map shows heavy traffic on Bronx River Parkway, do more work')
+"""
+Provide tutorial about traffic map conventions, e.g. red color means heavy traffic
+"""
+print('If last person in the office, lock door')
+print('If no "Out of Service" sign on elevator, press the elevator button')
+print('If "Out of service" sign, open door to stairwell\n\tdescend 4 flights\n\texit stairwell')
+print('Say good night to the security guard')
+print('If dark, find flashlight in purse and turn it on')
+print('Carefully walk to Twin Lakes parking lot.\nAvoid pot holes and speeding golf carts')
+print('Open car door and place bags in the back seat of car') # Diagram of car may be helpful here
+print('Turn on lights\n\tFind a good audiobook and press the "play" button\n\tIf phone battery dead, tune radio to WNYC')
+print("Arrive at "+ to_address)
---
+++ comments.py
@@ -0,0 +1,4 @@
+# test
+print('hello world')  # This is a comment
+#this is a test
+print('getting used to it')
