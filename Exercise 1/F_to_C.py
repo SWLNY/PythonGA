f83f57a9f98e99a12825b6402c8fd7cdcd31c2dc
SWLNY <lynchsusanw@gmail.com>
2019-03-25 19:10:40 -0400

    Comments were wrong. Fixed them.

--- Exercise 1/F_to_C.py
+++ Exercise 1/F_to_C.py
@@ -3,10 +3,10 @@
 Request a temperature in Fahrenheit. Convert it to the equivalent temperature in Celsius and print the result.
 """
 
-#  Prompt the user for a temperature in degrees Celsius.
+#  Prompt the user for a temperature in degrees Fahrenheit.
 f_temp = int(input("Please enter the temperature in degrees Fahrenheit. Enter as an integer.  "))
 
-#  Convert to the equivalent temperature in Fahrenheit.
+#  Convert to the equivalent temperature in Celsius.
 c_temp = (f_temp - 32) * 5 / 9
 
 #  Print the results.
