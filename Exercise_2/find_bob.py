e68adc37171191ded3d3cca7398c45c4db5b9887
SWLNY <lynchsusanw@gmail.com>
2019-04-05 19:24:38 -0400

    Added count bob program

---
+++ Exercise_2/find_bob.py
@@ -0,0 +1,16 @@
+"""
+2. Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print Number of times bob occurs is: 2
+Ask the user to input a string and then reversal the given input.
+Input: "Programming in Python"
+Output: nohtyP ni gnimmargorP
+"""
+
+bob_cnt = 0  # Counts the number of times 'bob' found
+input_str = input("Please enter the character string to search: ")
+
+for i in range(len(input_str) - 2):
+    #  print(input_str[i:i+3])
+    if input_str[i:i+3] == 'bob':  # Is the current three letter string 'bob'?
+        bob_cnt += 1  # If 'bob' found, increment counter.
+print("You entered: "+input_str)  # Inform the user
+print("Number of times 'bob' found: "+str(bob_cnt))
